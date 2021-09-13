33a7855cc9fa3cd04325799bf6ef313df1a75c16
ghadeer-078 <90647463+ghadeer-078@users.noreply.github.com>
2021-09-13 22:45:35 +0300

    Add files via upload

---
+++ Swift Fundamentals I.swift
@@ -0,0 +1,12 @@
+var type = "Rectangle"
+var description: String = "A quadrilateral with four right angles"
+var width: Double = 5
+var height: Double = 10.5
+//var area = width * height
+height += 1
+width += 1
+let area = width * height
+
+
+print("The shape is a \(type) or \(description) with an area of \(area)")
+
